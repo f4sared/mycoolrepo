a32c59e7151633a623101bf01c4f4a2efe8b4298
f4sared <f4sared@yahoo.com.sg>
2025-01-05 13:38:44 +0800

    okay

--- src/streamlit_llm_.py
+++ src/streamlit_llm_.py
@@ -18,9 +18,9 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    responsee = f"Echo: {prompt}"
+    response = f"Echo: {prompt}"
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(responsee)
+    with st.chat_message("assistantor"):
+        st.markdown(response)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": responsee})
+    st.session_state.messages.append({"role": "assistantor", "content": response})
