498e11098b970cd1e991b200a5cbccff536f5309
f4sared <f4sared@yahoo.com.sg>
2025-01-05 14:09:23 +0800

    update

--- src/streamlit_llm_.py
+++ src/streamlit_llm_.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import numpy as np
+import random
+import time
 
-st.title("Echo Bot")
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -12,20 +14,39 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    # without context
-    st.chat_message("user").markdown(prompt)
-    st.line_chart(np.random.randn(10, 1))
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
-    # with context
-    with st.chat_message("assistant"):
-        st.markdown(response)
-        st.line_chart(np.random.randn(10, 1))
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    response = st.write_stream(response_generator())
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+# # React to user input
+# if prompt := st.chat_input("What is up?"):
+#     # Display user message in chat message container
+#     # without context
+#     st.chat_message("user").markdown(prompt)
+#     # st.line_chart(np.random.randn(10, 1))
+#     # Add user message to chat history
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+
+#     response = f"Echo: {prompt}"
+#     # Display assistant response in chat message container
+#     # with context
+#     with st.chat_message("assistant"):
+#         st.markdown(response)
+#         # st.line_chart(np.random.randn(10, 1))
+#     # Add assistant response to chat history
+#     st.session_state.messages.append({"role": "assistant", "content": response})
