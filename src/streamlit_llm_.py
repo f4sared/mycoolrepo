e541a83dc3aa7637558649e96065260d21aabbae
f4sared <f4sared@yahoo.com.sg>
2025-01-05 13:48:59 +0800

    upgrade

--- src/streamlit_llm_.py
+++ src/streamlit_llm_.py
@@ -16,7 +16,7 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
-    st.line_chart(np.random.randn(30, 3))
+    st.line_chart(np.random.randn(10, 1))
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -24,6 +24,6 @@ if prompt := st.chat_input("What is up?"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
-        st.line_chart(np.random.randn(30, 3))
+        st.line_chart(np.random.randn(10, 1))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
