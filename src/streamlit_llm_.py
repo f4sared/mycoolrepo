827fa7b6f1b76fa911ba080b3b897050c68088f0
f4sared <f4sared@yahoo.com.sg>
2025-01-05 12:21:10 +0800

    update

--- src/streamlit_llm_.py
+++ src/streamlit_llm_.py
@@ -4,3 +4,7 @@ import numpy as np
 with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
+
+message = st.chat_message("user")
+message.write("Hello I am here")
+message.bar_chart(np.random.randn(30, 3))
