b9e5ab5eb56663e91c7e2bbfbd9f130de90e661e
f4sared <f4sared@yahoo.com.sg>
2025-01-05 13:46:42 +0800

    update

--- src/streamlit_llm_.py
+++ src/streamlit_llm_.py
@@ -16,6 +16,7 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
+    st.line_chart(np.random.randn(30, 3))
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
